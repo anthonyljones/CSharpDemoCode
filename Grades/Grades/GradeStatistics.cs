1abc8618267ff8172cf251337752f75df1567835
anthonyljones <35570444+anthonyljones@users.noreply.github.com>
2018-01-19 09:08:16 -0500

    Initial Upload

---
+++ Grades/Grades/GradeStatistics.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if (AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+                return result;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                string output;
+                switch (LetterGrade)
+                {
+
+
+                    case "A":
+                        output = "An A...Great Job!";
+                        break;
+                    case "B":
+                        output = "A B...Not Bad.";
+                        break;
+                    case "C":
+                        output = "A C...You can Do Better.";
+                        break;
+                    case "D":
+                        output = "A D...You are Underperforming.";
+                        break;
+                    case "F":
+                        output = "An F?!!!  Totally unacceptable!";
+                        break;
+                    default:
+                        output = "Your grade is incomplete.";
+                        break;
+                }
+
+                return output;
+
+            }
+            
+        }
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+
+
+}
+
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+            // synth.Speak("Hello! This is the grade book program");
+            IGradeTracker book = CreateGradeBook();
+
+            //GetBookName(book);
+            AddGrades(book);
+
+            SaveGrades(book);
+            WriteResults(book);
+
+            //synth.Speak("Average Grade is " + stats.AverageGrade);
+            //synth.Speak("Highest Grade is " + stats.HighestGrade);
+            //synth.Speak("Lowest Grade is " + stats.LowestGrade);
+        }
+
+        private static IGradeTracker CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(IGradeTracker book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+
+            foreach (float grade in book)
+            {
+                Console.WriteLine(grade);
+
+            }
+
+            WriteResult("Average Grade", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult("Grade", stats.LetterGrade);
+            Console.WriteLine(stats.Description);
+        }
+
+        private static void SaveGrades(IGradeTracker book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+
+                book.WriteGrades(outputFile);
+
+            }
+        }
+
+        private static void AddGrades(IGradeTracker book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(IGradeTracker book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+                Console.WriteLine(book.Name);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}" );
+        }
+    }
+}
